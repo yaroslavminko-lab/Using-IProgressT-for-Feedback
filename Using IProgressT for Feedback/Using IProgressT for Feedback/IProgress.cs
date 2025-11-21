f0c34bed31f187fd301fb6c6183067daafe65027
YarioM <yaroslav.minko@innowise.com>
2025-11-21 12:04:56 +0300

    Add project

---
+++ Using IProgressT for Feedback/Using IProgressT for Feedback/IProgress.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_IProgressT_for_Feedback
+{
+    interface IProgress
+    {
+    }
+}
